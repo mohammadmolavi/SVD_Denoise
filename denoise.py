d9202698616ead8a80d14c118360d4ada7300fa5
mohammadmolavi <mohammadmolavi97@gmail.com>
2023-06-23 20:44:53 +0330

    improve denoising

--- denoise.py
+++ denoise.py
@@ -1,12 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from skimage.transform import resize
+import time
 
+
+def take_first_k(v,k):
+    w = np.copy(v)
+    w[k:]=0
+    return(w)
 
 def svd(X):
     U, Sigma, V_transpose = np.linalg.svd(X)
-    return np.dot(np.dot(U[:,:17], np.diag(Sigma[:17])), V_transpose[:17,:])
+    T = take_first_k(Sigma, 35)
+    return U @ np.diag(T) @ V_transpose
 
 def denoise(img):
+    img= resize(img,(170,170))
     R = svd(img[:, :, 0])
     G = svd(img[:, :, 1])
     B = svd(img[:, :, 2])
